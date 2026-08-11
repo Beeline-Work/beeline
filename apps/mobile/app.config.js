77446146a16d729178b38b3c06f2c7ce4f3a7341
lunchboxfortwo <lunchboxfortwo@gmail.com>
2026-08-11 16:51:16 -0400

    Bump Android version code for v0.2.7 (#62)

--- apps/mobile/app.config.js
+++ apps/mobile/app.config.js
@@ -96,7 +96,7 @@ export default {
             }
         },
         android: {
-            versionCode: 8,
+            versionCode: 9,
             adaptiveIcon: {
                 foregroundImage: "./sources/assets/images/icon-adaptive.png",
                 monochromeImage: "./sources/assets/images/icon-monochrome.png",

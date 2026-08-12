c218888f56fc84c639ee5a0080957698460a843a
lunchboxfortwo <lunchboxfortwo@gmail.com>
2026-08-12 14:12:31 -0400

    chore: bump Android version code to 11 (#80)

--- apps/mobile/app.config.js
+++ apps/mobile/app.config.js
@@ -96,7 +96,7 @@ export default {
             }
         },
         android: {
-            versionCode: 10,
+            versionCode: 11,
             adaptiveIcon: {
                 foregroundImage: "./sources/assets/images/icon-adaptive.png",
                 monochromeImage: "./sources/assets/images/icon-monochrome.png",

09ff5398898e85d4b3a24f075a8361b21697c23f
lunchboxfortwo <lunchboxfortwo@gmail.com>
2026-08-21 11:22:04 -0400

    fix(mobile): enforce iOS display version format (#293)

--- apps/mobile/release-production.sh
+++ apps/mobile/release-production.sh
@@ -1,3 +1,4 @@
 set -e
+npm run version:check
 eas build --profile production --platform ios --auto-submit-with-profile=production --no-wait --non-interactive
 eas build --profile production --platform android --auto-submit-with-profile=production --no-wait --non-interactive
